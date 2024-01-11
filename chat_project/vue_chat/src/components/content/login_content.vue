ab040be52d539d88d8f17e57ff41e82b5ca12870
grp1011 <dkf1011vl@gmail.com>
2024-01-11 16:58:39 +0900

    채팅 프로젝트 front > 1.vue 버전 변경 2.로그인 화면 수정 3.proxy 추가 / 채팅 프로젝트 back > 1.스프링프레임워크 버전 변경

--- chat_project/vue_chat/src/components/content/login_content.vue
+++ chat_project/vue_chat/src/components/content/login_content.vue
@@ -1,24 +1,49 @@
 <template>
-  <div id="container">
-    <div id="loginBox">
-      <div id="loginBoxTitle">Login</div>
-      <div id="inputBox">
-        <div class="input-form-box">
-          <span>아이디 </span
-          ><input type="text" name="uid" class="form-control" />
-        </div>
-        <div class="input-form-box">
-          <span>비밀번호 </span
-          ><input type="password" name="upw" class="form-control" />
-        </div>
-        <div class="button-login-box">
-          <button
-            type="button"
-            class="btn btn-primary btn-xs"
-            style="width: 100%"
-          >
-            로그인
-          </button>
+  <div class="container mt-5">
+    <div class="row d-flex justify-content-center">
+      <div class="col-md-6">
+        <div class="card px-5 py-5" id="form1">
+          <div class="form-data" v-if="!submitted">
+            <div class="forms-inputs mb-4">
+              <span>Email</span>
+              <input
+                autocomplete="off"
+                type="text"
+                v-model="email"
+                v-bind:class="{ 'form-control': true }"
+                v-on:blur="emailBlured = true"
+              />
+              <div class="invalid-feedback">A valid email is required!</div>
+            </div>
+            <div class="forms-inputs mb-4">
+              <span>Password</span>
+              <input
+                autocomplete="off"
+                type="password"
+                v-model="password"
+                v-bind:class="{ 'form-control': true }"
+                v-on:blur="emailBlured = true"
+              />
+              <div class="invalid-feedback">Password must be 8 character!</div>
+            </div>
+            <div class="mb-3">
+              <button
+                v-on:click="login(email, password)"
+                class="btn btn-dark w-100"
+              >
+                Login
+              </button>
+            </div>
+          </div>
+          <div class="success-data" v-else>
+            <div class="text-center d-flex flex-column">
+              <i class="bx bxs-badge-check"></i>
+              <span class="text-center fs-1"
+                >You have been logged in <br />
+                Successfully</span
+              >
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -28,6 +53,49 @@
 <script>
 export default {
   name: "login_content",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
   components: {},
+  mounted() {
+    console.log("loginForm_content mounted 실행됨");
+  },
+  methods: {
+    async login(email, password) {
+      //formData형태로 시큐리티에 파라미터 넘기기
+      const loginData = new FormData();
+      loginData.append("email", email);
+      loginData.append("password", password);
+
+      this.axios(
+        {
+          method: "post",
+          url: "/api/login",
+          data: loginData,
+          Headers: {
+            "Context-Type": "multipart/form-data",
+          },
+        },
+        { withCredentials: true }
+      )
+        .then((response) => {
+          console.log("response.data = ", response.data);
+
+          if (response.data.email != null) {
+            console.log("/api/login 로그인 성공");
+            console.log("Login Successed");
+            // this.$router.push("/");
+          } else {
+            alert("Login Failed");
+          }
+        })
+        .finally(() => {
+          console.log("/api/login 종료");
+        });
+    },
+  },
 };
 </script>
--- chat_project/vue_chat/vue.config.js
+++ chat_project/vue_chat/vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+
+module.exports =defineConfig({
+    outputDir: "../vue_chat_sever/src/main/resources/static", //빌드 타깃 디렉터리
+    devServer: {
+        //프록시 설정
+        proxy:{
+            '^/api': { 
+                target: "http://localhost:8088",
+                changeOrigin:true,
+                ws:false,
+            },
+            '^/access': { 
+                target: "http://localhost:8088",
+                changeOrigin:true,
+                ws:false,
+            },
+        }
+    },
+}) ;
